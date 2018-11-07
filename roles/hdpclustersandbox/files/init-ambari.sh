beb885f9d2fa3ef0abed989e86f2027bda44df0e
Izhar Firdaus <kagesenshi.87@gmail.com>
2018-11-07 17:07:16 +0800

    deploy ranger

--- roles/hdpclustersandbox/files/init-ambari.sh
+++ roles/hdpclustersandbox/files/init-ambari.sh
@@ -5,4 +5,7 @@ set -e
 if [ ! -f "/var/ambari-init-executed" ]; then
    ambari-server setup -s
 fi
+echo "host all postgres 172.20.10.0/24 trust" >> /var/lib/pgsql/data/pg_hba.conf
+echo "host all rangeradmin 172.20.10.0/24 md5" >> /var/lib/pgsql/data/pg_hba.conf
+service postgresql reload
 echo $(date +%Y-%m-%d:%H:%M:%S) >> /var/ambari-init-executed
